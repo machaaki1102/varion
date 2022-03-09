90257b70e0c1849c8b2a17a3e31bed54ad2e2222
github <github>
2022-03-09 17:22:15 +0900

    ss

--- poetry_project/app.py
+++ poetry_project/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
+#import matplotlib
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from matplotlib.backends.backend_agg import RendererAgg
